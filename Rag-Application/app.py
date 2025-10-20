481981a4590fa890c18f63ac7f0fd4948640feec
Manoj-K <k_manoj@apple.com>
2025-10-20 18:45:32 +0530

    updated the code

--- Rag-Application/app.py
+++ Rag-Application/app.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_openai import ChatOpenAI
